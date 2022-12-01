bc669a71d7f106ac5f203ad029a3e492bbd74f06
Jatinsharma159 <jatinsh0803@gmail.com>
2022-12-01 17:50:40 +0530

    Authentication using passport, serialization and deserialization of cookie

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+
+const LocalStrategy = require('passport-local').Strategy;
+
+const User = require('../models/users');
+
+// Authentication using passport
+passport.use(new LocalStrategy({
+    usernameField: 'email'
+    }, 
+    function(email,password,done){   // done is callback function which is reporting back to passport js
+        // To find a user and establish identity
+        User.findOne({email:email}, function(err,user){    // {email(The one we are looking for in db) : email(The one which is passed)}
+            if(err){
+                console.log('Error while finding user ---> passport');
+                return done(err);
+            }
+            if(!user || user.password != password){
+                console.log('I navlid Username or Password');
+                return done(null,false);
+            } 
+
+            return done(null,user);
+        })
+
+    }));
+
+// serializing the user to decide which key is to be kept in cookies
+passport.serializeUser(function(user,done){      // We are basically setting cookie
+    done(null,user.id);
+});
+
+// deserializing the user from the key in the cookie
+passport.deserializeUser(function(id,done){    // We are finding the user from the cookie
+
+    User.findById(id,function(err,user){
+        if(err){
+            console.log('Error while finding user ---> passport');
+            return done(err);
+        }
+        return done(null,user);
+    });
+});
